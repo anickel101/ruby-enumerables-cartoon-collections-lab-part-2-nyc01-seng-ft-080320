d2c37530db252f56e9a86359c222d28e82d023f7
Anson Nickel <anson.nickel@gmail.com>
2020-07-20 13:42:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,3 @@ def find_valid_calls(planeteer_calls)
     valid_calls.any? {|y| y == x}
   end
 end
-
-
-  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |x| 
-    valid_calls.any? {|y| y == x}
-  end
-end
